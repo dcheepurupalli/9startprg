26aedd565aa18a049f0eefb2732203de10e64ff8
Dileep Cheepurupalli <dileep.cheepurupalli@sequoiacap.com>
2022-02-05 23:35:08 +0530

    Changes for header animation

--- components/header.js
+++ components/header.js
@@ -6,13 +6,21 @@ const Header = () => {
   useEffect(() => {
     const onScroll = () => {
       if (window.scrollY > 10) {
-        headerRef.current.classList.add("bg-white");
+        headerRef.current.classList.add("bg-[#FFFFFF]");
+        headerRef.current.classList.add("opacity-[0.8]");
+        headerRef.current.classList.add('backdrop-blur-xl');
+        headerRef.current.classList.add('backdrop-saturate-150');
         headerRef.current.classList.add("shadow-light");
         headerRef.current.classList.remove("text-white");
+        headerRef.current.classList.add("py-2");
       } else {
-        headerRef.current.classList.remove("bg-white");
+        headerRef.current.classList.remove("bg-[#FFFFFF]");
+        headerRef.current.classList.remove("opacity-[0.8]");
+        headerRef.current.classList.remove('backdrop-blur-xl');
+        headerRef.current.classList.remove('backdrop-saturate-150');
         headerRef.current.classList.remove("shadow-light");
         headerRef.current.classList.add("text-white");
+        headerRef.current.classList.remove("py-2");
       }
     };
 
@@ -24,8 +32,9 @@ const Header = () => {
 
   return (
     <header
-      className="body-font fixed z-10 w-full text-white transition"
+      className="body-font fixed z-10 w-full text-white transition duration-[250] ease-in-out py-4"
       onScroll={(e) => handleScroll(e)}
+      style={{ "transition": "all 0.3s ease-in-out 0s", "willChange": "transform", backdropFilter:"saturate(150%) blur(25px)"}}
       ref={headerRef}
     >
       <div className="mx-auto flex flex-col flex-wrap items-center p-5 md:flex-row">
@@ -33,13 +42,14 @@ const Header = () => {
           <img
             src="https://res.cloudinary.com/dsafp5nqg/image/upload/v1644055068/logo-crop_sbccyp.png"
             alt="logo"
+            className="h-10"
           />
         </a>
         <nav className="flex flex-wrap items-center justify-center text-base md:ml-auto">
-          <a className="mr-5 hover:text-gray-900">Buy</a>
-          <a className="mr-5 hover:text-gray-900">Sell</a>
-          <a className="mr-5 hover:text-gray-900">Search</a>
-          <a className="mr-5 hover:text-gray-900">Contact</a>
+          <a className="mr-16 hover:text-gray-400">Buy</a>
+          <a className="mr-16 hover:text-gray-400">Sell</a>
+          <a className="mr-16 hover:text-gray-400">Search</a>
+          <a className="mr-16 hover:text-gray-400">Contact</a>
         </nav>
       </div>
     </header>
